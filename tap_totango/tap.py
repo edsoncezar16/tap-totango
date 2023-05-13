25070dcc243542c131daa2f81db5064754a4d7c0
Edson Nogueira <edsoncezar16@gmail.com>
2023-05-13 19:18:09 -0300

    Working Accounts stream.

--- tap_totango/tap.py
+++ tap_totango/tap.py
@@ -32,7 +32,7 @@ class Taptotango(Tap):
             description="The token to authenticate against the API service",
         ),
         th.Property(
-            "terms",
+            "events_terms",
             th.ArrayType(
                 th.ObjectType(
                     th.Property("type", th.StringType, required=True),
@@ -41,17 +41,17 @@ class Taptotango(Tap):
             ),
             required=True,
             default=[],
-            description="An array containing filter conditions to use for the search.",
+            description="An array containing filter conditions to use for the events stream search.",
         ),
         th.Property(
-            "count",
+            "events_count",
             th.IntegerType,
             required=True,
             default=5,
-            description="The maximum number of accounts to return in the result set. The max. value for count is 1000.",
+            description="The maximum number of accounts to return in the events result set. The max. value for count is 1000.",
         ),
         th.Property(
-            "offset",
+            "events_offset",
             th.IntegerType,
             required=True,
             default=0,
@@ -62,6 +62,54 @@ class Taptotango(Tap):
             th.StringType,
             description="Filter the results for a specific account.",
         ),
+        th.Property(
+            "accounts_terms",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("type", th.StringType, required=True),
+                    additional_properties=True,
+                )
+            ),
+            required=True,
+            default=[],
+            description="An array containing filter conditions to use for the accounts stream search.",
+        ),
+        th.Property(
+            "accounts_fields",
+            th.ArrayType(
+                th.ObjectType(
+                    th.Property("type", th.StringType, required=True),
+                    additional_properties=True,
+                )
+            ),
+            required=True,
+            default=[],
+            description="List of fields to return as results. Note that the account name and account-id are always returned as well.",
+        ),
+        th.Property(
+            "accounts_count",
+            th.IntegerType,
+            default=1000,
+            description="The maximum number of accounts to return in the events result set. The max. value for count is 1000.",
+        ),
+        th.Property(
+            "accounts_offset",
+            th.IntegerType,
+            default=0,
+            description='Record number (0 states "start at record 0"). The record size can be defined using the count parameter (and limited to 1000). Tip: To page through results, ask for 1000 records (count: 1000). If you receive 1000 records, assume there’s more, in which case you want to pull the next 1000 records (offset: 1000…then 2000…etc.). Repeat paging until the number of records returned is less than 1000.',
+        ),
+        th.Property(
+            "accounts_sort_by",
+            th.StringType,
+            default="display_name",
+            description="Field name to sort the accounts stream results set by.",
+        ),
+        th.Property(
+            "accounts_sort_order",
+            th.StringType(allowed_values=["ASC", "DESC"]),
+            default="ASC",
+            description="Order to sort the accounts stream results set by.",
+        ),
     ).to_dict()
 
     def discover_streams(self) -> list[streams.totangoStream]:
@@ -70,7 +118,7 @@ class Taptotango(Tap):
         Returns:
             A list of discovered streams.
         """
-        return [streams.EventsStream(self)]
+        return [streams.EventsStream(self), streams.AccountsStream(self)]
 
 
 if __name__ == "__main__":
